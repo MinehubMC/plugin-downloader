d49c538ef5cf368ca0477d54b5c813cf2330a103
Midka <me@midka.dev>
2024-02-22 21:08:59 +0000

    feat: config file flag

--- cmd/root.go
+++ cmd/root.go
@@ -8,14 +8,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "pld",
-	Short: "Automatically download required plugins/dependencies for minecraft servers.",
-	Long:  "It reads a .json file and downloads the plugins to a specified folder. Created for easier creation of docker images.",
-	Run: func(cmd *cobra.Command, args []string) {
-		// do something
-		log.Default().Println("hello", args)
-	},
+var (
+	configFilePath string
+
+	rootCmd = &cobra.Command{
+		Use:   "pld",
+		Short: "Automatically download required plugins/dependencies for minecraft servers.",
+		Long:  "It reads a .json file and downloads the plugins to a specified folder. Created for easier creation of docker images.",
+		Run: func(cmd *cobra.Command, args []string) {
+			// do something
+			log.Default().Println("hello", args, configFilePath)
+		},
+	}
+)
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&configFilePath, "config", "", "config file (default is $PWD/dependencies.json)")
+
+	cobra.OnInitialize(onInit)
+}
+
+func onInit() {
+	if configFilePath == "" {
+		configFilePath = "dependencies.json"
+	}
 }
 
 func Execute() {
